ef5880d197396f78d459ff43c6d37130ff687d21
benbc <ben@bridesmere.com>
2010-04-24 21:20:05 +0100

    Separate pledge page.

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd website
-for n in bna_rules.doc contact.html index.html logo.jpg style.css surveys.html middle-temple.html
+for n in bna_rules.doc contact.html index.html logo.jpg style.css surveys.html middle-temple.html pledge.html
 do
     wget -N ftp://web18706:g5yuhwbj@www.barnurseryassociation.co.uk/website/$n
 done
