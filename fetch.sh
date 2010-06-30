ad3498dfbbdaf24178bc32c3d13076083a9626b8
benbc <ben@bridesmere.com>
2010-06-30 15:39:17 +0100

    Remove the proposal document.

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd website
-for n in bna_rules.doc contact.html index.html logo.jpg style.css surveys.html middle-temple.html pledge.html BNA_Proposal_April_2010.doc AnnexB.pdf AnnexC.pdf AnnexD.pdf
+for n in bna_rules.doc contact.html index.html logo.jpg style.css surveys.html middle-temple.html pledge.html
 do
     wget -N ftp://web18706:g5yuhwbj@www.barnurseryassociation.co.uk/website/$n
 done
