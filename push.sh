8b2622702d6e8e2193830c09bae951d87209a36d
benbc <ben@bridesmere.com>
2010-06-30 15:34:20 +0100

    Add Middle Temple proposal.

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd website
-for n in bna_rules.doc contact.html index.html logo.jpg style.css surveys.html middle-temple.html pledge.html
+for n in bna_rules.doc contact.html index.html logo.jpg style.css surveys.html middle-temple.html pledge.html BNA_Proposal_April_2010.doc AnnexB.pdf AnnexC.pdf AnnexD.pdf
 do
     wput --timestamping --dont-continue --reupload ftp://web18706:g5yuhwbj@www.barnurseryassociation.co.uk/website/$n
 done
